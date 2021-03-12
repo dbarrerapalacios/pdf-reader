6a21f20bf1281458d9da4764b39bd6ce32fba349
dbarrera <dbarrerapalacios@gmail.com>
2021-03-11 21:15:54 -0500

    no funciona el pause

--- src/js/pruebas.js
+++ src/js/pruebas.js
@@ -1,61 +1,91 @@
-document.getElementById("hablar").addEventListener("click", () => {
-  leerPdf(document.getElementById("libro").files, (texto) => {
-    leerTexto(texto);
-  });
-});
-const leerTexto = (texto) => {
-  speechSynthesis.speak(new SpeechSynthesisUtterance(texto));
-};
-const leerPdf = (e, callback) => {
-  if (e.length) {
-    console.log(e[0]);
-    var file = e[0];
-    var fileReader = new FileReader();
-    fileReader.onloadend = function () {
-      var typedarray = new Uint8Array(this.result);
-      var loadingTask = pdfjsLib.getDocument(typedarray);
-      loadingTask.promise.then(function (pdf) {
-        var maxPages = pdf.numPages;
-        var countPromises = [];
-        for (var j = 1; j <= maxPages; j++) {
-          var page = pdf.getPage(j);
-          var txt = "";
-          countPromises.push(
-            page.then(function (page) {
-              // add page promise
-              var textContent = page.getTextContent();
-              return textContent.then(function (text) {
-                // return content promise
-                return text.items
-                  .map(function (s) {
-                    return s.str;
-                  })
-                  .join(""); // value page text
-              });
-            })
-          );
-        }
-        return Promise.all(countPromises).then(function (texts) {
-          callback(texts.join(""));
-        });
-        // pdf.getPage(pdf.numPages).then((pages) => {
-        //     console.log(maxPages);
-        // you can now use *page* here
-        // var viewport = page.getViewport(2.0);
-        // var canvas = document.querySelector("canvas")
-        // canvas.height = viewport.height;
-        // canvas.width = viewport.width;
+const nombreArchivo = document.getElementById("nombre-archivo");
+var audio = new SpeechSynthesisUtterance();
+var synth = window.speechSynthesis;
 
-        // page.render({
-        // 	canvasContext: canvas.getContext('2d'),
-        // 	viewport: viewport
-        // });
-        // });
-      });
-    };
-    fileReader.readAsArrayBuffer(file);
-  } else {
-    console.log("no se selecciono archivo");
+document.getElementById("archivo").addEventListener("change", function () {
+  if (validar(this.files)) {
+    leerPdf(this.files, (texto) => {
+      leerTexto(texto);
+    });
   }
-  //   speechSynthesis.speak(new SpeechSynthesisUtterance(texto));
+});
+document.getElementById("reproducir").addEventListener("click", function () {
+  reproducir();
+});
+document.getElementById("pausar").addEventListener("click", function () {
+  pausar();
+});
+document.getElementById("reiniciar").addEventListener("click", function () {
+  reiniciar();
+});
+const validarNavegador = () => {
+  if (!"speechSynthesis" in window) {
+    nombreArchivo.textContent =
+      "Lo siento, tu navegador no soporta esta tecnología";
+  }
+};
+
+validarNavegador();
+const validar = (files) => {
+  if (files.length === 0) {
+    nombreArchivo.textContent = "Debe selecciona un archivo";
+    return false;
+  }
+  nombreArchivo.textContent = files[0].name;
+  return true;
+};
+const leerTexto = (texto) => {
+  console.log(texto);
+  if (texto.length > 0) {
+    audio.text = texto;
+  } else {
+    nombreArchivo.textContent = "No se encontro texto en el archivo";
+  }
+};
+
+const reproducir = () => {
+  if (!synth.pending && !synth.speaking) {
+    console.log("reprocuir");
+    synth.speak(audio);
+  }
+};
+
+const pausar = () => {
+  synth.pause();
+};
+const reiniciar = () => {
+  synth.cancel();
+};
+
+const leerPdf = (e, callback) => {
+  var file = e[0];
+  var fileReader = new FileReader();
+  fileReader.onloadend = function () {
+    var typedarray = new Uint8Array(this.result);
+    var loadingTask = pdfjsLib.getDocument(typedarray);
+    loadingTask.promise.then(function (pdf) {
+      var maxPages = pdf.numPages;
+      var countPromises = [];
+      for (var j = 1; j <= maxPages; j++) {
+        var page = pdf.getPage(j);
+        var txt = "";
+        countPromises.push(
+          page.then(function (page) {
+            var textContent = page.getTextContent();
+            return textContent.then(function (text) {
+              return text.items
+                .map(function (s) {
+                  return s.str;
+                })
+                .join("");
+            });
+          })
+        );
+      }
+      return Promise.all(countPromises).then(function (texts) {
+        callback(texts.join(""));
+      });
+    });
+  };
+  fileReader.readAsArrayBuffer(file);
 };
